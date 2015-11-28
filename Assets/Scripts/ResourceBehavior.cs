ab1edebee566d9973074d661bb194d6fcc328abc
Sam Kasura <kasurs@rpi.edi>
2015-11-28 01:04:01 -0500

    Three different Cube colors and variable for player to be able to tell the difference between them.

--- Assets/Scripts/ResourceBehavior.cs
+++ Assets/Scripts/ResourceBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ResourceBehavior : MonoBehaviour {
 
     public float fallRate;
+    public string resourceType;
     float translation = 0;
     // Use this for initialization
     void Start () {
