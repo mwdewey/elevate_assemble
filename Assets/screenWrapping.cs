23bceb89abfe79bd928c435bf604e3f24d1701cb
Farkinator <thefarkinator@gmail.com>
2015-12-03 18:54:36 -0500

    ScreenWrapping. Need to move the boundaries up with the player.

--- Assets/screenWrapping.cs
+++ Assets/screenWrapping.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class screenWrapping : MonoBehaviour {
     //Whether or not the attached trigger is for the left or right side of the screen.
     public bool isLeftBoundary;
+    //The other boundary 
     public GameObject oppositeBound;
+    //How far the player is displaced from the other boundary after a screen wrap.
+    public float bound_offset;
     private GameObject player;
+    
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
@@ -22,6 +26,11 @@ public class screenWrapping : MonoBehaviour {
 
     void OnTriggerEnter(Collider hit) {
         Vector3 wrappedPos = hit.gameObject.transform.position;
+        if (isLeftBoundary) {
+            wrappedPos.x = oppositeBound.transform.position.x - bound_offset;
+        } else {
+            wrappedPos.x = oppositeBound.transform.position.x + bound_offset;
+        }
         hit.gameObject.transform.position = wrappedPos;
     }
 }
