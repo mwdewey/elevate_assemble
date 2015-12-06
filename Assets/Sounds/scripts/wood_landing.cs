599eb30c5d1692683da18729424f9658633cf6da
Michael Dewey <deweym@rpi.edu>
2015-12-06 00:13:53 -0500

    Implemented sounds for falling resources.

---
+++ Assets/Sounds/scripts/wood_landing.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wood_landing : MonoBehaviour {
+
+    AudioSource source;
+    public AudioClip clip;
+
+    public int maxPlays = 1;
+    int currentPlays = 0;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (currentPlays < maxPlays)
+        {
+            float vol = Random.Range(0, 5f);
+            source.pitch = Random.Range(0.75f, 1.25f);
+
+            source.PlayOneShot(clip, vol);
+            currentPlays++;
+        }
+    }
+}
