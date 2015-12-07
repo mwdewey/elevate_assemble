8dea0563ae76dc62324dcf8433ddabf975fe5dbe
Michael Dewey <deweym@rpi.edu>
2015-12-07 10:16:02 -0500

    Added web sockets. Replaced http polling.

--- Assets/Scripts/WebSpawner.cs
+++ Assets/Scripts/WebSpawner.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Net;
 using SimpleJSON;
+using WebSocketSharp;
 
 public class WebSpawner : MonoBehaviour {
 
-    string url = "http://dvm.io/api/getObjects.php";
-
-    public bool run;
     public float minObjRate;  // objects per second
     float timeSinceLastObj;
-    bool prevB;
-    Worker w;
-    Thread workerThread;
+    WebSocket ws;
+    List<ResourceObject> resources;
+    float prevPos;
 
     public GameObject rock;  // index 1
     public GameObject grass; // index 2
@@ -22,165 +20,112 @@ public class WebSpawner : MonoBehaviour {
 
     void Start()
     {
-        if (run) startLoop();
-        prevB = run;
         timeSinceLastObj = 0;
+        resources = new List<ResourceObject>();
+        prevPos = transform.position.x;
+
+        ws = new WebSocket("ws://dvm.io:8081/socket.io/?EIO=3&transport=websocket");
+        ws.OnError += (sender, e) => Debug.Log("ERROR: " + e.Message);
+        ws.OnMessage += (sender, e) => onMessageReceived(sender,e);
+        ws.ConnectAsync();
+    }
+
+    void onMessageReceived(object sender, MessageEventArgs e)
+    {
+        try
+        {
+            var json = JSON.Parse(e.Data.Substring(2));
+
+            string tag = json.AsArray[0];
+            JSONNode objJson = json.AsArray[1];
+
+            switch (tag) {
+                // from web client
+                case "obj" :
+                    int objType = objJson["id"].AsInt;
+                    int position = objJson["pos"].AsInt;
+                    resources.Add(new ResourceObject(objType, position));
+                    break;
+                // from unity, so this would be other unity clients
+                case "pos" :
+                    break;
+                default :
+                    break;
+            }
+
+          
+        }
+
+        catch (System.Exception error) {/* Debug.log(error.StackTrace) */ }
+
     }
 	
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            run = !run;
-        }
-
-        // if run just changed
-        if (run != prevB)
-        {
-            // start or end loop
-            if (run) startLoop();
-            else w.stopTask();
-        }
-
-        prevB = run;
 
         // if any objects exist in queue, spawn
-        if (w != null)
+        foreach (ResourceObject resObj in resources)
         {
-            
-            foreach (ResourceObject resObj in w.getResources())
+            timeSinceLastObj = 0;
+            Debug.Log(resObj.objType + " " + resObj.position);
+
+            Vector3 pos = new Vector3(map((float)resObj.position, 0, 100f, -6f, 6f),transform.position.y + 7, -5);
+            Quaternion rot;
+            GameObject objTemp;
+
+            switch (resObj.objType)
             {
-                timeSinceLastObj = 0;
-                Debug.Log(resObj.objType + " " + resObj.position);
-
-                Vector3 pos = new Vector3(map((float)resObj.position, 0, 100f, -6f, 6f), 7, -5);
-                Quaternion rot;
-                GameObject objTemp;
-
-                switch (resObj.objType)
-                {
-                    // rock
-                    case 1:
-                        rot = Quaternion.Euler(-90, 0, 0);
-                        objTemp = Instantiate(rock, pos, rot) as GameObject;
-                        objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
-                        break;
-                    // grass
-                    case 2:
-                        rot = Quaternion.Euler(-90, 0, 0);
-                        objTemp = Instantiate(grass, pos, rot) as GameObject;
-                        objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
-                        break;
-                    // wood
-                    case 3:
-                        rot = Quaternion.Euler(0, 0, 0);
-                        objTemp = Instantiate(wood, pos, rot) as GameObject;
-                        objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
-                        break;
-                    default: break;
-                }
+                // rock
+                case 1:
+                    rot = Quaternion.Euler(-90, 0, 0);
+                    objTemp = Instantiate(rock, pos, rot) as GameObject;
+                    objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
+                    break;
+                // grass
+                case 2:
+                    rot = Quaternion.Euler(-90, 0, 0);
+                    objTemp = Instantiate(grass, pos, rot) as GameObject;
+                    objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
+                    break;
+                // wood
+                case 3:
+                    rot = Quaternion.Euler(0, 0, 0);
+                    objTemp = Instantiate(wood, pos, rot) as GameObject;
+                    objTemp.GetComponent<ResourceBehavior>().UIhandler = this.UIHandler;
+                    break;
+                default: 
+                    break;
             }
-            w.getResources().Clear();
-
-            // local object spawner if web is insufficient
-            if (timeSinceLastObj > 1f / minObjRate)
-            {
-                w.getResources().Add(new ResourceObject(Random.Range(1, 3), Random.Range(0, 100)));
-
-                timeSinceLastObj = 0;
-            }
-
-            timeSinceLastObj += Time.deltaTime;
         }
+        resources.Clear();
+
+        // local object spawner if web is insufficient
+        if (timeSinceLastObj > 1f / minObjRate)
+        {
+            resources.Add(new ResourceObject(Random.Range(1, 3), Random.Range(0, 100)));
+
+            timeSinceLastObj = 0;
+        }
+        timeSinceLastObj += Time.deltaTime;
+
+        // player tracker
+        if (prevPos != transform.position.x)
+        {
+            ws.SendAsync("42[\"pos\",\"" + (transform.position.x + 6) + "\"]",null);
+        }
+        prevPos = transform.position.x;
+        
 
 	}
-
-    void startLoop()
-    {
-        w = new Worker(url);
-        workerThread = new Thread(w.task);
-        workerThread.Start();
-    }
 
     float map(float s, float a1, float a2, float b1, float b2)
     {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
 
-    void OnDisable()
-    {
-        if (w != null) w.stopTask();
-    }
-
     void OnDestroy()
     {
-        if (w != null) w.stopTask();
+        if (ws != null) ws.CloseAsync();
     }
-}
-
-class Worker
-{
-    string resource_url;
-    bool run;
-    List<ResourceObject> resources;
-    WebClient client;
-
-    public Worker(string _resource_url)
-    {
-        resource_url = _resource_url;
-        run = true;
-        resources = new List<ResourceObject>();
-        client = new WebClient();
-        client.Headers.Clear();
-
-        // clear any old resources in DB
-        getResourcesWeb();
-        resources.Clear();
-    }
-
-    public void task()
-    {
-        while (run)
-        {
-            getResourcesWeb();
-        }
-
-    }
-
-    public void stopTask()
-    {
-        run = false;
-    }
-
-    void getResourcesWeb()
-    {
-        string returnText = client.DownloadString(resource_url);
-
-        var json = JSON.Parse(returnText);
-
-        foreach (JSONNode obj in json.AsArray)
-        {
-            string objType = obj["objType"];
-            string position = obj["pos"];
-
-            int objTypeInt = -1;
-            int positionInt = -1;
-
-            if(int.TryParse(objType, out objTypeInt) &&
-                int.TryParse(position, out positionInt))
-            {
-                resources.Add(new ResourceObject(objTypeInt, positionInt));
-            }
-
-
-        }
-
-    }
-
-    public List<ResourceObject> getResources()
-    {
-        return resources;
-    }
-
 }
 
 class ResourceObject
---
+++ Assets/Scripts/socks.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+
+public class socks : MonoBehaviour {
+
+    WebSocket ws;
+    float prevPos;
+
+	void Start () {
+        ws = new WebSocket("ws://dvm.io:8081/socket.io/?EIO=3&transport=websocket");
+
+        //ws.OnError += (sender, e) => Debug.Log("ERROR: " + e.Message);
+
+        ws.ConnectAsync();
+
+        prevPos = transform.position.x;
+	}
+
+    void Update ()
+    {
+        if (prevPos != transform.position.x)
+        {
+            ws.Send("42[\"pos\",\"" + (transform.position.x + 6) + "\"]");
+        }
+        prevPos = transform.position.x;
+
+    }
+
+    void OnDestroy()
+    {
+        if (ws != null) ws.Close();
+    }
+}
