ba28bd16b0f8d0b3a76002ae7899e366a3be21e9
Michael Dewey <deweym@rpi.edu>
2015-12-08 13:48:53 -0500

    TTL script and updated prefabs.

---
+++ Assets/Scripts/ttl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ttl : MonoBehaviour {
+
+    public float timeToLive;
+    float spawnTime;
+
+	void Start () {
+        spawnTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTime += Time.deltaTime;
+
+        if (spawnTime > timeToLive) Destroy(this.gameObject);
+	}
+}
