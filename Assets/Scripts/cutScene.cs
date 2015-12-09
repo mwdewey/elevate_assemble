26051ced0079f2ada53310327495068c5385c996
Michael Dewey <deweym@rpi.edu>
2015-12-09 12:20:33 -0500

    Added cutscenes to game.

---
+++ Assets/Scripts/cutScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cutScene : MonoBehaviour {
+
+    float ScreenWidth;
+    float ScreenHeight;
+    GameObject cutscene;
+
+
+	void Start () {
+        ScreenWidth = Screen.width;
+        ScreenHeight = Screen.height;
+        cutscene = transform.Find("cutscene").gameObject;
+
+        cutscene.GetComponent<RectTransform>().sizeDelta = new Vector2(ScreenWidth, ScreenHeight);
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F)) Destroy(cutscene);
+	}
+}
